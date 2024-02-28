29f88e586413c425c78b60c198eedfe283e851e2
joelcardoso95 <joelmarcos.cardoso@aol.com>
2024-02-28 14:38:28 -0300

    commit inicial

---
+++ variables/inteiras.go
@@ -0,0 +1,13 @@
+package variables
+
+import "fmt"
+
+func MostrarInteiros() {
+	var intcomum int
+	int32 := int32(10)
+	int64 := int64(100)
+
+	fmt.Println("intcomum = ", intcomum)
+	fmt.Println("int32 = ", int32)
+	fmt.Println("int64 = ", int64)
+}
